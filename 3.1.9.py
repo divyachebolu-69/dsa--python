ffceea9e81775bd1d59bb2693d04153542517394
karthikvch <divyachebolu2@gmail.com>
2026-09-03 18:45:44 +0530

    Added 3.1.9.py program

---
+++ 3.1.9.py
@@ -0,0 +1,19 @@
+class solution:
+    def pattern11(self, N):
+        for i in range(N):
+            if i % 2 == 0:
+                start = 1
+            else:
+                start = 0
+
+            for j in range(i + 1):
+                print(start, end="")
+                start = 1 - start
+
+            print()
+
+
+sol = solution()
+N = 9
+sol.pattern11(N)                   
+   
